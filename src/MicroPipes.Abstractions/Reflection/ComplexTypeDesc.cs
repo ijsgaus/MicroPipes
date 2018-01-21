8e864247ad44cc59357dedb6b5f4aae1ff74a32f
IJSGaus <ijsgaus@gmail.com>
2018-01-21 23:26:34 +0300

    Refactorung

---
+++ src/MicroPipes.Abstractions/Reflection/ComplexTypeDesc.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using JetBrains.Annotations;
+
+namespace MicroPipes.Reflection
+{
+    public abstract class ComplexTypeDesc : TypeDesc
+    {
+        public ComplexTypeDesc([NotNull] QualifiedIdentifier name, IEnumerable<(NameAndIndex, TypeMemberDesc)> members) : base(name)
+        {
+            Members = members.ToImmutableDictionary();
+        }
+
+        public IReadOnlyDictionary<NameAndIndex, TypeMemberDesc> Members { get; }
+
+        protected internal override bool Equals(TypeDesc other)
+        {
+            return base.Equals(other) && ((ComplexTypeDesc) other).Members.DefaultComparerEqual(Members);
+        }
+
+        public override int GetHashCode()
+        {
+            return (base.GetHashCode() * 397) ^ Members.DefaultComparerHashCode();
+        }
+    }
+}
--- src/MicroPipes.Abstractions/Reflection/RecordDesc.cs
+++ src/MicroPipes.Abstractions/Reflection/RecordDesc.cs
@@ -4,41 +4,9 @@ using JetBrains.Annotations;
 
 namespace MicroPipes.Reflection
 {
-    public abstract class ComplexTypeDesc : TypeDesc
-    {
-        public ComplexTypeDesc([NotNull] QualifiedIdentifier name, IEnumerable<(NameAndIndex, TypeMemberDesc)> members) : base(name)
-        {
-            Members = members.ToImmutableDictionary();
-        }
-
-        public IReadOnlyDictionary<NameAndIndex, TypeMemberDesc> Members { get; }
-
-        protected internal override bool Equals(TypeDesc other)
-        {
-            return base.Equals(other) && ((ComplexTypeDesc) other).Members.DefaultComparerEqual(Members);
-        }
-
-        public override int GetHashCode()
-        {
-            return (base.GetHashCode() * 397) ^ Members.DefaultComparerHashCode();
-        }
-    }
-    
     public sealed class RecordDesc : ComplexTypeDesc
     {
         public RecordDesc([NotNull] QualifiedIdentifier name, IEnumerable<(NameAndIndex, TypeMemberDesc)> members) : base(name, members)
-        {
-        }
-
-        internal override void Anchor()
-        {
-            
-        }
-    }
-
-    public sealed class OneOfType : ComplexTypeDesc
-    {
-        public OneOfType([NotNull] QualifiedIdentifier name, IEnumerable<(NameAndIndex, TypeMemberDesc)> members) : base(name, members)
         {
         }
 
